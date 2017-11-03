22de5e8b9ff8913433a9e92dbdd17be6ed489fc9
IvanovaKateryna <kis.iv333@gmail.com>
2017-11-03 13:18:51 +0200

    Update data in modul Test and Openacademy

--- test/models/models.py
+++ test/models/models.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-
+from datetime import datetime
 
 class Tests(models.Model):
     _name = 'test.test'
@@ -39,29 +39,9 @@ class TestResPartner(models.Model):
 
     is_tester = fields.Boolean('Is Tester', readonly=True,
                                store=False, compute='_compute_is_tester',)
-
-    tests_expected = fields.Integer('Tests Expected Within 30 days',
-                                    readonly=True,
-                                    store=False,
-                                    compute='_compute_tests_expected')
+    tests_expected = fields.One2many('test.test_session', 'test')
 
     @api.one
     def _compute_is_tester(self):
         self.is_tester = bool(self.env['test.test'].
                               search_count([('tester', '=', self.id)]))
-
-    @api.one
-    def _compute_tests_expected(self):
-        query = '''
-                  SELECT count(tts.id)
-                  FROM test_test_session as tts
-                    INNER JOIN test_test as tt
-                      on tts.test = tt.id
-                    INNER JOIN res_partner as rp
-                      on tt.tester = rp.id
-                  WHERE tt.tester = %s
-                    AND tts.start_date < NOW() + INTERVAL %s
-                    AND tts.end_date > NOW()
-               '''
-        self.env.cr.execute(query, [self.id, '1 month'])
-        self.tests_expected = self.env.cr.fetchone()[0]
